15e509e7673cb9f38246937f20732744ed68f819
DorinVieru <dorino.vieru@gmail.com>
2024-04-29 16:40:54 +0200

    UP: Write function for view adresses in a List of Address & print in Console

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -6,7 +6,9 @@
         {
             // Path del file
             string path = "C:\\Users\\User\\OneDrive - vxw2g\\Desktop\\CORSO BOOLEAN\\ESERCICIZI CORSO BOOLEAN\\.NET\\csharp-lista-indirizzi\\addresses.csv";
-            
+            var indirizzo = LeggiIndirizzoDaCSV(path);
+            foreach (var ind in indirizzo)
+                Console.WriteLine(ind.ToString());
         }
 
          public static List<Indirizzi> LeggiIndirizzoDaCSV(string path)
@@ -14,7 +16,36 @@
             List<Indirizzi> indirizzo = new List<Indirizzi>();
             var stream = File.OpenText(path);
 
-            
+            int i = 0;
+            while (!stream.EndOfStream)
+            {
+                var linea = stream.ReadLine();
+
+                i++;
+                if (i <= 1) // Ignoro la prima riga del testo
+                    continue;
+
+                try
+                {
+                    var dati = linea.Split(',');
+                    string name = dati[0];
+                    string surname = dati[1];
+                    string street = dati[2];
+                    string city = dati[3];
+                    string province = dati[4];
+                    int zip = int.Parse(dati[5]);
+
+                    Indirizzi ind = new Indirizzi(name, surname, street, city, province, zip);
+                    indirizzo.Add(ind);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Si è verificato un errore durante la lettura dell'indirizzo: {e.Message}");
+                }
+            }
+
+            stream.Dispose();
+            return indirizzo;
         }
     }
 }
