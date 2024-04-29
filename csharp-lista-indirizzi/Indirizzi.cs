50cf293f4b42cfe39c9d087443255225ec1081b5
DorinVieru <dorino.vieru@gmail.com>
2024-04-29 15:24:30 +0200

    UP: Create class of Addresses

---
+++ csharp-lista-indirizzi/Indirizzi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzi
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public int ZIP { get; set; }
+
+        public Indirizzi (string name, string surname, string street, string city, string province, int zip)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.ZIP = zip;
+        }
+    }
+}
